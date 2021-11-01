1004fde3c2c195c24f3b7bfa0732fcd1b80526dc
Kamil <herbetko.kamil@gmail.com>
2021-11-01 18:06:46 +0100

    "Changing mergesort to make it stable. Also, adding tests to tasks 1 - 4."

--- src/Lista3/lista3.ml
+++ src/Lista3/lista3.ml
@@ -3,14 +3,36 @@
 (* Zadanie 2 *)
 let curry3 f a b c = f (a, b, c);;
 let curry3 = function f -> function a -> function b -> function c -> f (a, b, c);;
+(* curry3 : ('a * 'b * 'c -> 'd) -> 'a -> 'b -> 'c -> 'd = <fun> *)
+
+let plus (x1, x2, x3) = x1 + x2 + x3;;
+let concat3 (x1, x2, x3) = x1 ^ x2 ^ x3;;
+
+let curriedPlus = curry3(plus);;
+let curriedConcat3 = curry3(concat3);;
+
+curriedPlus 1 2 3 = 6;;
+curriedConcat3 "a" "b" "c" = "abc";;
 
 let uncurry3 f (a, b, c) = f a b c;;
 let uncurry3 = function f -> function (a, b, c) -> f a b c;;
+(* uncurry3 : ('a -> 'b -> 'c -> 'd) -> 'a * 'b * 'c -> 'd = <fun> *)
+
+let plus x1 x2 x3 = x1 + x2 + x3;;
+let concat3 x1 x2 x3 = x1 ^ x2 ^ x3;;
+
+let uncurriedPlus = uncurry3 plus;;
+let uncurriedConcat3 = uncurry3 concat3;;
+
+uncurriedPlus(1, 2, 3) = 6;;
+uncurriedConcat3("a", "b", "c") = "abc";;
 
 (* Zadanie 3 *)
 let sumProd xs = List.fold_left (fun (sum, prod) -> fun h -> (sum + h, prod * h)) (0, 1) xs;;
 
 sumProd [1; 2; 3; 4] = (10, 24);;
+sumProd [] = (0, 1);;
+sumProd [4; 3; 2; 1] = (10, 24);;
 
 (* Zadanie 5 *)
 (* a *)
@@ -35,11 +57,10 @@ insertionsort (fun (x1, y1) (x2, y2) -> x1 < x2) [(6, 3); (6, 2); (1, 2); (4, 2)
 (* b *)
 
 
-let rec halfsplit xs =
-    match xs with
-    | [] -> ([], [])
-    | h1 :: h2 :: t -> let (l1, l2) = halfsplit t in (h1 :: l1, h2 :: l2)
-    | h :: [] -> ([h], []);;
+let rec halfsplit n xs =
+    match (n, xs) with
+    | (0, xs) -> ([], xs)
+    | n, h :: t -> let (l1, l2) = halfsplit (n - 1) t in (h :: l1, l2);;
 
 let rec merge pred (xs, ys) =
     match (xs, ys) with
@@ -51,20 +72,14 @@ let rec mergesort pred xs =
     match xs with
         | [] -> []
         | [x] -> [x]
-        | h :: t as l1 -> let (h1, h2) = halfsplit l1 in merge pred (mergesort pred h1, mergesort pred h2);;
+        | h :: t as l1 -> let (h1, h2) = halfsplit (List.length l1 / 2) l1 in merge pred (mergesort pred h1, mergesort pred h2);;
 
 
-mergesort (<) [5; 4; 4; 2; 5; 3] = [2; 3; 4; 4; 5; 5];;
-mergesort (<) ["zs"; "ab1"; "dc"; "cd"] =  ["ab1"; "cd"; "dc"; "zs"];;
+mergesort (<=) [5; 4; 4; 2; 5; 3] = [2; 3; 4; 4; 5; 5];;
+mergesort (<=) ["zs"; "ab1"; "dc"; "cd"] =  ["ab1"; "cd"; "dc"; "zs"];;
 mergesort (fun (x1, y1) (x2, y2) -> x1 <= x2) [(6, 2); (6, 3); (4, 2); (1, 3); (1, 2)] = [(1, 3); (1, 2); (4, 2); (6, 2); (6, 3)];;
 mergesort (fun (x1, y1) (x2, y2) -> x1 <= x2) [(6, 3); (6, 2); (1, 2); (4, 2); (1, 3)] = [(1, 2); (1, 3); (4, 2); (6, 3); (6, 2)];;
 
-[(6,2), (4,2), (1,2)] [(6,3), (1,3)]
-[(6,2), (4,2)] [(1,2)], [(6,3)], [(1,3)]
-[(6, 2)], [(4, 2)], [(1, 2)], [(6,3)], [(1,3)]
-[(4, 2), (6,2)], [(1, 2)], [(1,3), (6,3)]
-[(1, 2), (4,2), (6,2)], [(1,3), (6,3)]
-[(1, 2), (1,3), (4, 2), (6,2), (6,3)]
 
 
 
